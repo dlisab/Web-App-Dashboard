f0ba4a8f94ebd85b38f44ee25984c3f0ba41234a
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-04-19 10:21:25 -0400

    reverting

--- js/charts.js
+++ js/charts.js
@@ -16,7 +16,7 @@ let trafficData = {
 let trafficOptions = {
     backgroundColor: 'rgba(112, 104, 201, .5)',
     fill: true,
-    maintainAspectRatio: true,
+    maintainAspectRatio: false,
     animation: {
     duration: 0
     },
