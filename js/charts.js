907d8b10a7440a84443ebf4298e3aa3c28a44a64
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-04-23 08:50:12 -0400

    revert

--- js/charts.js
+++ js/charts.js
@@ -7,14 +7,16 @@ const weekly = document.querySelector('.weeklyt');
 const monthly = document.querySelector('.monthlyt');
 
 
-var chartData = {
-	labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-	datasets: [{
-		backgroundColor: 'rgba(116, 120, 191, .5)',
-		borderColor: 'rgba(116, 120, 191, 1)',
-		data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
-	}]
-};
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    }]
+    };
 let trafficOptions = {
     backgroundColor: 'rgba(112, 104, 201, .5)',
     fill: true,
